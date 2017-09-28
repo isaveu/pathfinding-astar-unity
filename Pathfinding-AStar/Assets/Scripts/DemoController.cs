f10a62d9004b247cdb4cdbdc6c3c7dd95d9c105e
maydinunlu <maydinunlu@gmail.com>
2017-09-28 22:34:37 +0300

    GraphView
    Signed-off-by: maydinunlu <maydinunlu@gmail.com>

---
+++ Pathfinding-AStar/Assets/Scripts/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DemoController : MonoBehaviour {
+
+    public MapData mapData;
+    public Graph graph;
+
+    /**************************************************/
+
+    private void Start() {
+        if (mapData != null && graph != null) {
+            int[,] mapInstance = mapData.MakeMap();
+            graph.Init(mapInstance);
+
+            GraphView graphView = graph.gameObject.GetComponent<GraphView>();
+            if (graphView != null) {
+                graphView.Init(graph);
+            }
+        }
+    }
+
+}
---
+++ Pathfinding-AStar/Assets/Scripts/GraphView.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Graph))]
+public class GraphView : MonoBehaviour {
+
+    public GameObject NodeViewPrefab;
+
+    public Color baseColor = Color.white;
+    public Color wallColor = Color.black;
+
+    /**************************************************/
+
+    public void Init(Graph graph) {
+        if (graph == null) {
+            Debug.LogWarning("GRAPHVIEW No graph to initialize");
+            return;
+        }
+
+        foreach (Node n in graph.nodes) {
+            GameObject instance = Instantiate(NodeViewPrefab, Vector3.zero, Quaternion.identity);
+            NodeView nodeView = instance.GetComponent<NodeView>();
+            if (nodeView != null) {
+                nodeView.Init(n);
+
+                if (n.nodeType == NodeType.Blocked) {
+                    nodeView.ColorNode(wallColor);
+                } else {
+                    nodeView.ColorNode(baseColor);
+                }
+            }
+        }
+    }
+
+}
